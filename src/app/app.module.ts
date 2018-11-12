c875141cd254c6c687d4232a51dfe52df0a0b511
Panu Ervamaa <panu.ervamaa@frantic.com>
2018-11-12 11:46:04 +0200

    enable ivy with diff per --experimental-ivy option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
